842f2c6cfd43c316036d7981ffe8da2ef09bea22
JayrieL24 <119652189+JayrieL24@users.noreply.github.com>
2024-03-26 11:24:00 +0800

    Update act5.asm

--- MP5_Acuña/act5.asm
+++ MP5_Acuña/act5.asm
@@ -1,1 +1,54 @@
+section .bss
+    buffer resb 1
 
+section .text
+    global _start
+
+_start:
+    
+    mov eax, 3           
+    mov ebx, 0           
+    mov ecx, buffer      
+    mov edx, 1          
+    int 80h             
+
+    cmp byte [buffer], 'a'
+    jl char_isnot_uppercase
+    cmp byte [buffer], 'z'
+    jg char_isnot_uppercase
+
+    sub byte [buffer], 32 
+
+    mov eax, 4           
+    mov ebx, 1          
+    mov ecx, buffer      
+    mov edx, 1           
+    int 80h 
+    jmp stop
+
+    char_isnot_uppercase:
+
+    cmp byte [buffer], 'A'
+    jl not_char
+    cmp byte [buffer], 'Z'
+    jg not_char
+
+    add byte [buffer], 32
+
+
+not_char:
+
+    mov eax, 4           
+    mov ebx, 1          
+    mov ecx, buffer      
+    mov edx, 1           
+    int 80h      
+
+stop:
+    mov eax, 1
+
+  ; sudo apt-get update
+  ; sudo apt-get -y install nasm
+  ; nasm -f elf act5.asm  
+  ; ld -m  elf_i386 act5.o -o act5
+  ; ./act5
